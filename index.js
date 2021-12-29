fb5b2a6bbb6decb3d9823fa7f716f7795cd9cbc7
Marina Zinchenko <iolsoofuolso@gmail.com>
2021-12-28 19:24:07 -0800

    set eslint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-'use strict';
-import _ from 'lodash';
-console.log('Gvenya and Spiral and Rosie and Marmu = <3');
-const t = {};
+// 'use strict';
+// import _ from 'lodash';
 
-console.log(_.last([23, 45]));
+// console.log('Gvenya and Spiral and Rosie and Marmu = <3');
+const t = (val) => val / 2;
+t();
+// console.log(_.last([23, 45]));
+
+const readThis = (val) => val;
+readThis();
